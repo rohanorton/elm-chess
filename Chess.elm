b0f5435efa33f602c46659a70ad8a7afd915aabc
Rohan Orton <rohan.orton@gmail.com>
2016-11-22 14:06:26 +0000

    Upgrade to 0.18

--- Chess.elm
+++ Chess.elm
@@ -1,159 +1,235 @@
-module Chess (..) where
+module Chess exposing (..)
 
-import String
-import Maybe.Extra
 import Dict
 import Char
+import Html exposing (Html, table, tbody, tr, td, span, text)
+import Html.Attributes as Attr
+
+
+main : Html a
+main =
+    toHtml startingBoard
+
+
+
+-- Board Types
 
 
 type alias Board =
-  List (List Square)
+    List (List Square)
 
 
 type alias Square =
-  Maybe Piece
+    Maybe Piece
 
 
 type Piece
-  = Piece PColor PType
+    = Piece Colour Figure
 
 
-type PColor
-  = White
-  | Black
+type Colour
+    = White
+    | Black
 
 
-type PType
-  = Queen
-  | King
-  | Bishop
-  | Rook
-  | Knight
-  | Pawn
+type Figure
+    = Queen
+    | King
+    | Bishop
+    | Rook
+    | Knight
+    | Pawn
 
 
-showPiece : Piece -> Char
-showPiece piece =
-  case piece of
-    Piece White Queen ->
-      'Q'
 
-    Piece White King ->
-      'K'
-
-    Piece White Bishop ->
-      'B'
-
-    Piece White Rook ->
-      'R'
-
-    Piece White Knight ->
-      'N'
-
-    Piece White Pawn ->
-      'P'
-
-    Piece Black Queen ->
-      'q'
-
-    Piece Black King ->
-      'k'
-
-    Piece Black Bishop ->
-      'b'
-
-    Piece Black Rook ->
-      'r'
-
-    Piece Black Knight ->
-      'n'
-
-    Piece Black Pawn ->
-      'p'
+-- Read Board
 
 
-typeList : Dict.Dict Char PType
-typeList =
-  Dict.fromList
-    [ ( 'p', Pawn )
-    , ( 'n', Knight )
-    , ( 'r', Rook )
-    , ( 'b', Bishop )
-    , ( 'k', King )
-    , ( 'q', Queen )
-    ]
+figureList : Dict.Dict Char Figure
+figureList =
+    Dict.fromList
+        [ ( 'p', Pawn )
+        , ( 'n', Knight )
+        , ( 'r', Rook )
+        , ( 'b', Bishop )
+        , ( 'k', King )
+        , ( 'q', Queen )
+        ]
 
 
-readPiece : Char -> Maybe Piece
-readPiece char =
-  let
-    color =
-      if Char.isUpper char then
-        White
-      else
-        Black
-
-    pieceType =
-      Dict.get (Char.toLower char) typeList
-
-    makePiece =
-      Piece color
-  in
-    Maybe.map makePiece pieceType
+readSquare : Char -> Square
+readSquare char =
+    let
+        colour =
+            if (Char.isUpper char) then
+                Black
+            else
+                White
+    in
+        figureList
+            |> Dict.get (Char.toLower char)
+            |> Maybe.map (Piece colour)
 
 
-showSquare : Square -> Char
-showSquare square =
-  case square of
-    Nothing ->
-      '.'
-
-    Just piece ->
-      showPiece piece
+readRow : String -> List Square
+readRow =
+    List.map readSquare << String.toList
 
 
-readSquare : Char -> Maybe Square
-readSquare c =
-  case c of
-    '.' ->
-      Just Nothing
-
-    _ ->
-      Maybe.map Just (readPiece c)
+readBoard : String -> Board
+readBoard =
+    List.map readRow << String.split "\n"
 
 
-readBoard : String -> Maybe Board
-readBoard str =
-  let
-    readRow =
-      Maybe.Extra.traverse readSquare
-  in
-    String.lines str
-      |> List.map String.toList
-      |> Maybe.Extra.traverse readRow
+startingBoardString : String
+startingBoardString =
+    String.join "\n"
+        [ "rnbqkbnr"
+        , "pppppppp"
+        , "........"
+        , "........"
+        , "........"
+        , "........"
+        , "PPPPPPPP"
+        , "RNBQKBNR"
+        ]
 
 
-showBoard : Board -> String
-showBoard board =
-  let
-    showRow =
-      List.map showSquare
-  in
-    List.map showRow board
-      |> List.map String.fromList
-      |> String.join "\n"
+startingBoard : Board
+startingBoard =
+    readBoard startingBoardString
 
 
-initialBoardStr : String
-initialBoardStr =
-  String.join
-    "\n"
-    [ "rnbqkbnr"
-    , "pppppppp"
-    , "........"
-    , "........"
-    , "........"
-    , "........"
-    , "PPPPPPPP"
-    , "RNBQKBNR"
-    ]
+
+-- View
+
+
+toHtml : Board -> Html a
+toHtml board =
+    let
+        children =
+            board
+                |> List.indexedMap rowView
+    in
+        table
+            [ Attr.style
+                [ "text-align" => "center"
+                , "border-spacing" => "0pt"
+                , "border-collapse" => "collapse"
+                , "border-color" => "silver"
+                , "border-style" => "solid"
+                , "border-width" => "0pt 0pt 0pt 0pt"
+                ]
+            ]
+            [ tbody [] children
+            ]
+
+
+rowView : Int -> List Square -> Html a
+rowView x squares =
+    let
+        children =
+            squares
+                |> List.indexedMap (squareView x)
+    in
+        tr
+            [ Attr.style
+                [ "vertical-align" => "bottom"
+                ]
+            ]
+            children
+
+
+squareView : Int -> Int -> Square -> Html a
+squareView x y square =
+    td
+        [ Attr.style
+            [ "width" => "40pt"
+            , "height" => "40pt"
+            , "background-color" => colourByPosition ( x, y )
+            ]
+        ]
+        [ pieceView square ]
+
+
+pieceView : Square -> Html a
+pieceView square =
+    let
+        maybeString =
+            square
+                |> Maybe.map pieceToUnicode
+    in
+        case maybeString of
+            Nothing ->
+                text ""
+
+            Just piece ->
+                span [ Attr.style [ "font-size" => "250%" ] ]
+                    [ text piece
+                    ]
+
+
+pieceToUnicode : Piece -> String
+pieceToUnicode piece =
+    case piece of
+        Piece Black King ->
+            "♔"
+
+        Piece White King ->
+            "♚"
+
+        Piece Black Queen ->
+            "♕"
+
+        Piece White Queen ->
+            "♛"
+
+        Piece Black Rook ->
+            "♖ "
+
+        Piece White Rook ->
+            "♜"
+
+        Piece Black Bishop ->
+            "♗"
+
+        Piece White Bishop ->
+            "♝"
+
+        Piece Black Knight ->
+            "♘"
+
+        Piece White Knight ->
+            "♞"
+
+        Piece Black Pawn ->
+            "♙"
+
+        Piece White Pawn ->
+            "♟"
+
+
+colourByPosition : ( Int, Int ) -> String
+colourByPosition ( x, y ) =
+    let
+        silver =
+            x + y |> isOdd
+    in
+        if silver then
+            "silver"
+        else
+            "white"
+
+
+
+-- Utilities
+
+
+(=>) : String -> String -> ( String, String )
+(=>) =
+    (,)
+
+
+isOdd : Int -> Bool
+isOdd x =
+    x % 2 /= 0
